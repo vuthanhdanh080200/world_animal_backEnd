f1bd799cd7834e7e21337663e1daac4afd782401
Danh <Danh@LAPTOP-FDQSAODR>
2021-08-14 23:06:01 +0700

    make it better

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,9 @@
 module.exports = {
-  user: "sa",
-  password: "123456789",
-  database: "world_animal",
-  server: "localhost",
+  user: "danh",
+  password: "123456abcABC",
+  database: "world_animal_test",
+  server: "mssql-44725-0.cloudclusters.net",
+  port: 19777,
   pool: {
     max: 10,
     min: 0,
